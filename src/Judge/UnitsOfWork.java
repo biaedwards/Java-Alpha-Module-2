522f44362e98951fd9949d8fe79f3334cdf9c39b
bilyana <bibby.georgieva@gmail.com>
2018-05-30 16:19:50 +0300

    A bit faster.

--- src/Judge/UnitsOfWork.java
+++ src/Judge/UnitsOfWork.java
@@ -6,70 +6,76 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class UnitsOfWork {
-    static HashMap<String, Unit> units = new HashMap<>();
-    static final Comparator<Unit> cmp = Comparator.comparingInt(Unit::getAttack).reversed().thenComparing(Unit::getName);
-    static PriorityQueue<Unit> pq = new PriorityQueue<>(cmp);
+    private static HashMap<String, Unit> units = new HashMap<>();
+    private static HashMap<String, HashSet<Unit>> types = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         while (parse(in.readLine().split(" "))) ;
     }
 
-    static void addUnit(String name, String type, int attack) {
+    private static void addUnit(String name, String type, int attack) {
         if (units.containsKey(name)) {
             System.out.printf("FAIL: %s already exists!\n", name);
         } else {
             Unit temp = new Unit(name, type, attack);
             units.put(name, temp);
-            pq.add(temp);
+            if (types.containsKey(type)) {
+                types.get(type).add(temp);
+            } else {
+                HashSet<Unit> hs = new HashSet<>();
+                hs.add(temp);
+                types.put(type, hs);
+            }
             System.out.printf("SUCCESS: %s added!\n", name);
         }
     }
 
-    static void removeUnit(String name) {
+    private static void removeUnit(String name) {
         if (!units.containsKey(name)) {
             System.out.printf("FAIL: %s could not be found!\n", name);
         } else {
+            Unit temp = units.get(name);
             units.remove(name);
-            for (Unit unit : pq) {
-                if (unit.name.equals(name)) {
-                    pq.remove(unit);
-                    break;
-                }
-            }
+            types.get(temp.type).remove(temp);
             System.out.printf("SUCCESS: %s removed!\n", name);
         }
     }
 
-    static void findType(String type) {
+    private static void findType(String type) {
         System.out.print("RESULT: ");
-        PriorityQueue<Unit> pqCopy = new PriorityQueue<>(pq);
-        while (!pqCopy.isEmpty()) {
-            Unit temp = pqCopy.poll();
-            if (temp.type.equals(type)){
-                System.out.print(temp);
-                if (!pqCopy.isEmpty()) {
-                    System.out.print(", ");
-                }
-            }
+        StringBuilder sb = new StringBuilder();
+        if(!types.containsKey(type)){
+            System.out.println();
+            return;
         }
-        System.out.println();
+        types.get(type).stream()
+                .sorted()
+                .forEach(x -> sb.append(x).append(", "));
+        if (sb.length() != 0) {
+            sb.delete(sb.length() - 2, sb.length());
+            System.out.println(sb);
+        } else System.out.println();
     }
 
-    static void power(int number) {
+    private static void power(int number) {
         System.out.print("RESULT: ");
-        PriorityQueue<Unit> pqCopy = new PriorityQueue<>(pq);
-        while (number > 0 && !pqCopy.isEmpty()) {
-            --number;
-            System.out.print(pqCopy.poll());
-            if (!pqCopy.isEmpty()) {
-                System.out.print(", ");
-            }
+        if(units.isEmpty()){
+            System.out.println();
+            return;
         }
-        System.out.println();
+        StringBuilder sb = new StringBuilder();
+        units.values().stream()
+                .sorted()
+                .limit(number)
+                .forEach(x -> sb.append(x).append(", "));
+        if (sb.length() != 0) {
+            sb.delete(sb.length() - 2, sb.length());
+            System.out.println(sb);
+        } else System.out.println();
     }
 
-    static boolean parse(String[] input) {
+    private static boolean parse(String[] input) {
         switch (input[0]) {
             case "add":
                 addUnit(input[1], input[2], Integer.parseInt(input[3]));
@@ -89,7 +95,7 @@ public class UnitsOfWork {
         return true;
     }
 
-    static class Unit {
+    static class Unit implements Comparable{
         private String name;
         private String type;
         private int attack;
@@ -115,6 +121,13 @@ public class UnitsOfWork {
         @Override
         public String toString() {
             return String.format("%s[%s](%d)", name, type, attack);
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Unit unit = (Unit) o;
+            if(Integer.compare(this.attack, unit.attack)!=0) return -Integer.compare(this.attack, unit.attack);
+            return this.name.compareTo(unit.name);
         }
     }
 }
