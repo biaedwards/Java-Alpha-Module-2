e36ad44d9351ae05731ffc2e18c9e9f0f172a094
bilyana <bibby.georgieva@gmail.com>
2018-06-04 15:35:29 +0300

    Slogan solved

--- src/DNSExam/Slogan.java
+++ src/DNSExam/Slogan.java
@@ -1,4 +1,193 @@
 package DNSExam;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+
 public class Slogan {
+    static class InputReader {
+
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        InputReader() {
+            this.stream = System.in;
+        }
+
+        int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        int readInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        long readLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        double readDouble() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.' && c != ',') {
+                if (c == 'e' || c == 'E') {
+                    return res * Math.pow(10, readInt());
+                }
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.' || c == ',') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E') {
+                        return res * Math.pow(10, readInt());
+                    }
+                    if (c < '0' || c > '9') {
+                        throw new InputMismatchException();
+                    }
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        String readLine() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        //        boolean isSpaceChar(int c) {
+//            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+//        }
+        boolean isSpaceChar(int c) {
+            return c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        OutputWriter() {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        }
+
+        void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        void close() {
+            writer.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader();
+        OutputWriter out = new OutputWriter();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.readInt();
+        HashSet<String> use = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] words = in.readLine().split(" ");
+            use.addAll(Arrays.asList(words));
+            String word = in.readLine();
+            sb.append(split(use, word, "")).append("\n");
+        }
+        out.print(sb);
+        out.close();
+    }
+
+    private static String split(HashSet<String> use, String word, String result) {
+        if (use.contains(word)) return result + word;
+        for (int i = 1; i < word.length(); i++) {
+            if (use.contains(word.substring(0, i))){
+                String remaining = split(use, word.substring(i), result + word.substring(0,i) + " ");
+                if (!remaining.equals("NOT VALID")) return remaining;
+            }
+        }
+        return "NOT VALID";
+    }
 }
